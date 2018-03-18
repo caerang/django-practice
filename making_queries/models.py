411fe7100de843f4d3054ae479e002cb4f6fce54
Hyeongdo Lee <mylovercorea@gmail.com>
2018-03-19 01:40:37 +0900

    Add making queries

--- making_queries/models.py
+++ making_queries/models.py
@@ -2,12 +2,14 @@
 
 from django.db import models
 
+
 class Blog(models.Model):
     name = models.CharField(max_length=100)
     tagline = models.TextField()
 
     def __str__(self):
         return self.name
+
 
 class Author(models.Model):
     name = models.CharField(max_length=200)
@@ -16,8 +18,9 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+
 class Entry(models.Model):
-    blog = models.ForeignKey(Blog)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     headline = models.CharField(max_length=255)
     body_text = models.TextField()
     pub_date = models.DateField()
